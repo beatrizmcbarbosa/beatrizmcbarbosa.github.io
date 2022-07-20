10a084e5e8fef6e5072e109c1e40e732de1c1d99
Beatriz Barbosa <biabarbosa98@gmail.com>
2022-07-20 11:22:48 +0100

    Clock JS

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// Date & Time
+
+let now = new Date();
+let dateTime = document.querySelector("#date-time");
+let hour = now.getHours();
+let minute = now.getMinutes;
+if (minute<10){
+    minute='0${minute}';
+}
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let day = days[now.getDay()];
+let date = now.getDate();
+let months = ["Jan",
+"Feb",
+"March",
+"Apr",
+"May",
+"Jun",
+"Jul",
+"Aug",
+"Sept",
+"Oct",
+"Nov",
+"Dec",];
+let month = months[now.getMonth];
+let year = now.getFullYear();
+dateTime.innerHTML = 'Last updated at ${hour}:${minute}, on ${day}, ${month} ${date}, ${year}'
