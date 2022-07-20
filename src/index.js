9c0dc74ce40e1697580b3a086bf79696b34bfc3e
Beatriz Barbosa <biabarbosa98@gmail.com>
2022-07-20 11:30:54 +0100

    Update date JS

--- src/index.js
+++ src/index.js
@@ -3,23 +3,25 @@
 let now = new Date();
 let dateTime = document.querySelector("#date-time");
 let hour = now.getHours();
-let minute = now.getMinutes;
-
-let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let minute = now.getMinutes();
+if (minute<10){
+    minute=`0${minute}`;
+}
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let day = days[now.getDay()];
 let date = now.getDate();
-let months = ["Jan",
-"Feb",
+let months = ["January",
+"February",
 "March",
-"Apr",
+"April",
 "May",
-"Jun",
-"Jul",
-"Aug",
-"Sept",
-"Oct",
-"Nov",
-"Dec",];
-let month = months[now.getMonth];
+"Junr",
+"July",
+"August",
+"September",
+"October",
+"November",
+"December",];
+let month = months[now.getMonth()];
 let year = now.getFullYear();
 dateTime.innerHTML = `Last updated at ${hour}:${minute}, on ${day}, ${month} ${date}, ${year}`;
